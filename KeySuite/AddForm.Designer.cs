20174cc22d0414aa47d7d2dd04887515fc7d9240
Dylan Bowman <dylanbowmandsk@gmail.com>
2019-04-06 04:04:39 +0100

    added textbox and labels to addform

--- KeySuite/AddForm.Designer.cs
+++ KeySuite/AddForm.Designer.cs
@@ -28,20 +28,200 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cdKeyLabel = new System.Windows.Forms.Label();
+            this.productLabel = new System.Windows.Forms.Label();
+            this.supplierLabel = new System.Windows.Forms.Label();
+            this.distributorLabel = new System.Windows.Forms.Label();
+            this.steamUrlLabel = new System.Windows.Forms.Label();
+            this.g2aUrlLabel = new System.Windows.Forms.Label();
+            this.regionLabel = new System.Windows.Forms.Label();
+            this.cdTextBox = new System.Windows.Forms.TextBox();
+            this.productTextBox = new System.Windows.Forms.TextBox();
+            this.supplierTextBox = new System.Windows.Forms.TextBox();
+            this.distributorTextBox = new System.Windows.Forms.TextBox();
+            this.steamUrlTextBox = new System.Windows.Forms.TextBox();
+            this.g2aUrlTextBox = new System.Windows.Forms.TextBox();
+            this.regionTextBox = new System.Windows.Forms.TextBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // cdKeyLabel
+            // 
+            this.cdKeyLabel.AutoSize = true;
+            this.cdKeyLabel.Location = new System.Drawing.Point(13, 13);
+            this.cdKeyLabel.Name = "cdKeyLabel";
+            this.cdKeyLabel.Size = new System.Drawing.Size(43, 13);
+            this.cdKeyLabel.TabIndex = 0;
+            this.cdKeyLabel.Text = "CD Key";
+            // 
+            // productLabel
+            // 
+            this.productLabel.AutoSize = true;
+            this.productLabel.Location = new System.Drawing.Point(12, 43);
+            this.productLabel.Name = "productLabel";
+            this.productLabel.Size = new System.Drawing.Size(44, 13);
+            this.productLabel.TabIndex = 1;
+            this.productLabel.Text = "Product";
+            // 
+            // supplierLabel
+            // 
+            this.supplierLabel.AutoSize = true;
+            this.supplierLabel.Location = new System.Drawing.Point(13, 70);
+            this.supplierLabel.Name = "supplierLabel";
+            this.supplierLabel.Size = new System.Drawing.Size(45, 13);
+            this.supplierLabel.TabIndex = 2;
+            this.supplierLabel.Text = "Supplier";
+            // 
+            // distributorLabel
+            // 
+            this.distributorLabel.AutoSize = true;
+            this.distributorLabel.Location = new System.Drawing.Point(12, 97);
+            this.distributorLabel.Name = "distributorLabel";
+            this.distributorLabel.Size = new System.Drawing.Size(54, 13);
+            this.distributorLabel.TabIndex = 3;
+            this.distributorLabel.Text = "Distributor";
+            // 
+            // steamUrlLabel
+            // 
+            this.steamUrlLabel.AutoSize = true;
+            this.steamUrlLabel.Location = new System.Drawing.Point(13, 122);
+            this.steamUrlLabel.Name = "steamUrlLabel";
+            this.steamUrlLabel.Size = new System.Drawing.Size(62, 13);
+            this.steamUrlLabel.TabIndex = 4;
+            this.steamUrlLabel.Text = "Steam URL";
+            // 
+            // g2aUrlLabel
+            // 
+            this.g2aUrlLabel.AutoSize = true;
+            this.g2aUrlLabel.Location = new System.Drawing.Point(13, 148);
+            this.g2aUrlLabel.Name = "g2aUrlLabel";
+            this.g2aUrlLabel.Size = new System.Drawing.Size(53, 13);
+            this.g2aUrlLabel.TabIndex = 5;
+            this.g2aUrlLabel.Text = "G2A URL";
+            // 
+            // regionLabel
+            // 
+            this.regionLabel.AutoSize = true;
+            this.regionLabel.Location = new System.Drawing.Point(13, 174);
+            this.regionLabel.Name = "regionLabel";
+            this.regionLabel.Size = new System.Drawing.Size(41, 13);
+            this.regionLabel.TabIndex = 6;
+            this.regionLabel.Text = "Region";
+            // 
+            // cdTextBox
+            // 
+            this.cdTextBox.Location = new System.Drawing.Point(105, 13);
+            this.cdTextBox.Name = "cdTextBox";
+            this.cdTextBox.Size = new System.Drawing.Size(176, 20);
+            this.cdTextBox.TabIndex = 8;
+            // 
+            // productTextBox
+            // 
+            this.productTextBox.Location = new System.Drawing.Point(105, 40);
+            this.productTextBox.Name = "productTextBox";
+            this.productTextBox.Size = new System.Drawing.Size(176, 20);
+            this.productTextBox.TabIndex = 9;
+            // 
+            // supplierTextBox
+            // 
+            this.supplierTextBox.Location = new System.Drawing.Point(105, 67);
+            this.supplierTextBox.Name = "supplierTextBox";
+            this.supplierTextBox.Size = new System.Drawing.Size(113, 20);
+            this.supplierTextBox.TabIndex = 10;
+            // 
+            // distributorTextBox
+            // 
+            this.distributorTextBox.Location = new System.Drawing.Point(105, 93);
+            this.distributorTextBox.Name = "distributorTextBox";
+            this.distributorTextBox.Size = new System.Drawing.Size(113, 20);
+            this.distributorTextBox.TabIndex = 11;
+            // 
+            // steamUrlTextBox
+            // 
+            this.steamUrlTextBox.Location = new System.Drawing.Point(105, 119);
+            this.steamUrlTextBox.Name = "steamUrlTextBox";
+            this.steamUrlTextBox.Size = new System.Drawing.Size(224, 20);
+            this.steamUrlTextBox.TabIndex = 12;
+            // 
+            // g2aUrlTextBox
+            // 
+            this.g2aUrlTextBox.Location = new System.Drawing.Point(105, 145);
+            this.g2aUrlTextBox.Name = "g2aUrlTextBox";
+            this.g2aUrlTextBox.Size = new System.Drawing.Size(224, 20);
+            this.g2aUrlTextBox.TabIndex = 13;
+            // 
+            // regionTextBox
+            // 
+            this.regionTextBox.Location = new System.Drawing.Point(105, 171);
+            this.regionTextBox.Name = "regionTextBox";
+            this.regionTextBox.Size = new System.Drawing.Size(59, 20);
+            this.regionTextBox.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(105, 197);
+            this.cancelButton.Name = "button1";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 15;
+            this.cancelButton.Text = "button1";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.addButton.Location = new System.Drawing.Point(186, 197);
+            this.addButton.Name = "button2";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 16;
+            this.addButton.Text = "button2";
+            this.addButton.UseVisualStyleBackColor = true;
             // 
             // AddForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(341, 267);
+            this.ClientSize = new System.Drawing.Size(341, 231);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.regionTextBox);
+            this.Controls.Add(this.g2aUrlTextBox);
+            this.Controls.Add(this.steamUrlTextBox);
+            this.Controls.Add(this.distributorTextBox);
+            this.Controls.Add(this.supplierTextBox);
+            this.Controls.Add(this.productTextBox);
+            this.Controls.Add(this.cdTextBox);
+            this.Controls.Add(this.regionLabel);
+            this.Controls.Add(this.g2aUrlLabel);
+            this.Controls.Add(this.steamUrlLabel);
+            this.Controls.Add(this.distributorLabel);
+            this.Controls.Add(this.supplierLabel);
+            this.Controls.Add(this.productLabel);
+            this.Controls.Add(this.cdKeyLabel);
             this.Name = "AddForm";
             this.Text = "AddForm";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AddForm_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label cdKeyLabel;
+        private System.Windows.Forms.Label productLabel;
+        private System.Windows.Forms.Label supplierLabel;
+        private System.Windows.Forms.Label distributorLabel;
+        private System.Windows.Forms.Label steamUrlLabel;
+        private System.Windows.Forms.Label g2aUrlLabel;
+        private System.Windows.Forms.Label regionLabel;
+        private System.Windows.Forms.TextBox cdTextBox;
+        private System.Windows.Forms.TextBox productTextBox;
+        private System.Windows.Forms.TextBox supplierTextBox;
+        private System.Windows.Forms.TextBox distributorTextBox;
+        private System.Windows.Forms.TextBox steamUrlTextBox;
+        private System.Windows.Forms.TextBox g2aUrlTextBox;
+        private System.Windows.Forms.TextBox regionTextBox;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button addButton;
     }
 }
